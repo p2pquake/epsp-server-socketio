7cdda42c7ec9bf30969d68fb96079dfaca48e767
typewriter <takuyaxp@gmail.com>
2019-06-21 15:00:33 +0900

    Add express http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("hello, world!");
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (_, res) => res.send('Hello world!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
