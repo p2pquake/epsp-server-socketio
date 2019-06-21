b90f1af662e292d5ab4e7e6cf41f20b751b7dae5
typewriter <takuyaxp@gmail.com>
2019-06-21 15:27:25 +0900

    Add Socket.IO server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import socketio, { Socket } from 'socket.io';
+import http from 'http';
 
-app.get('/', (_, res) => res.send('Hello world!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// configuration
+const port = process.env.PORT || 3000;
+
+// initialize
+const app = express();
+let server = http.createServer(app);
+let io = socketio(server);
+
+// mount
+app.get('/', (_, res) => {
+  res.send('Hello world!')
+});
+
+io.on('connection', (socket) => {
+  console.log('connected');
+  socket.emit('example', { hello: 'world' });
+  socket.on('other event', (data) => {
+    console.log(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
