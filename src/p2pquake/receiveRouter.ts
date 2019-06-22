07a64dc2a52619acabe38a2333f57307159b5231
typewriter <takuyaxp@gmail.com>
2019-06-22 11:43:06 +0900

    Implement authentication

--- src/p2pquake/receiveRouter.ts
+++ src/p2pquake/receiveRouter.ts
@@ -3,23 +3,28 @@ import express from 'express';
 import Broadcaster from './broadcaster';
 
 export default class ReceiveRouter {
+  private key: string;
   private broadcaster: Broadcaster;
 
   public constructor(server: socketio.Server) {
     this.broadcaster = new Broadcaster(server);
+    this.key = process.env.EPSP_RECEIVE_ROUTER_KEY || '';
+    
+    if (!this.key) {
+      throw "Key not defined";
+    }
   }
 
   public router(): express.Router {
     const router = express.Router();
     router.post('/broadcast', (req, res) => {
-      // FIXME: No authentication
+      const key = req.body["key"];
+      if (key != this.key) { res.status(401).end(); return; }
 
-      if (!req.body["code"]) {
-        res.status(400).end();
-        return;
-      }
+      const data = req.body["data"];
+      if (!data) { res.status(400).end(); return; }
 
-      this.broadcaster.broadcast(req.body["code"].toString(), req.body);
+      this.broadcaster.broadcast(data["code"].toString(), data);
       res.end();
     });
     return router;
